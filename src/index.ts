598fe1d297ba8a9dd3f32b40fb44da4b33c8ccf2
Udhaykrishn <krishnauday320@gmail.com>
2025-02-01 15:01:56 +0530

    refactor(index): database connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import app from "@/app"
-
+import { DatabaseService } from "./config"
 const bootstrap = async () => {
+    await DatabaseService.connection()
     app.listen(3001, () => console.log(`🚀 Server started on ${3001}`))
 }
 
