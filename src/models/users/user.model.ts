bebeea46da36cf193e7c845c5628f150aef107ab
Udhaykrishn <krishnauday320@gmail.com>
2025-02-06 10:06:36 +0530

    feat: create user model

--- src/models/users/user.model.ts
+++ src/models/users/user.model.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-import { IUser } from "@/models/users/interface/user.interface";
+import { IUser } from "./interface";
 
 const UserSchema = new Schema<IUser>({
     email: {
@@ -55,6 +55,21 @@ const UserSchema = new Schema<IUser>({
 
 UserSchema.index({ email: 1 }, { unique: true }),
     UserSchema.index({ username: 1 })
+
+UserSchema.pre("save", async function (next) {
+    const vendor = this as IUser;
+
+    if (!vendor.isModified("password")) return next(); // Only hash if password is modified
+
+
+    this.password = this.password // replace with password hashing method
+    try {
+
+        next();
+    } catch (error: any) {
+        return next(error);
+    }
+});
 
 const User = mongoose.model<IUser>("Users", UserSchema)
 export default User
