c45a8b4cfa53e6291e089aecf87da29febc4abd9
Udhaykrishn <krishnauday320@gmail.com>
2025-02-10 10:37:04 +0530

    feat: added blocked property for understand and block the cateogyrs

--- src/models/admins/interface/category.interface.ts
+++ src/models/admins/interface/category.interface.ts
@@ -2,10 +2,10 @@ import { Document, Types } from "mongoose";
 
 
 export interface ICategory extends Document {
-    _id: string;
     description: string;
     subCategory: Types.ObjectId[];
     createdAt: Date;
     updatedAt: Date;
     name: string;
+    isBlocked:boolean
 }
