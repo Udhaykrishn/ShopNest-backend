8b5afbfc8c2837a9e8267790ccc46785850d2420
Udhaykrishn <krishnauday320@gmail.com>
2025-02-10 10:28:36 +0530

    feat(container): update the container and add more services in container file

--- src/config/inversify.config.ts
+++ src/config/inversify.config.ts
@@ -1,9 +1,28 @@
 import { Container } from "inversify";
-import { TYPES } from "@/types"
+import { VENDOR, USER, CATEGORY } from "@/types"
 import { UserController } from "@/controllers/users/user.controller";
+import { VendorController } from "@/controllers/vendor/vendor.controller";
+import { VendorRepository } from "@/repository/vendor/vendor.repository";
+import { VendorService } from "@/services/vendor/vendor.service";
+import { PRODUCT } from "@/types/product.types";
+import { ProductController } from "@/controllers/vendor/products.controller";
+import { ProductService } from "@/services/vendor/product.services";
+import { ProductRepository } from "@/repository/vendor/product.repository";
+import { CategoryRespository } from "@/repository/admin/category.repository";
+import { CategoryService } from "@/services/admin/category.service";
+import { CategoryController } from "@/controllers/admin/category.controller";
 
 const container = new Container();
 
-container.bind<UserController>(TYPES.UserController).to(UserController)
+container.bind<UserController>(USER.UserController).to(UserController)
+container.bind<VendorController>(VENDOR.VendorController).to(VendorController)
+container.bind<VendorRepository>(VENDOR.VendorRepository).to(VendorRepository)
+container.bind<VendorService>(VENDOR.VendorService).to(VendorService)
+container.bind<ProductController>(PRODUCT.ProductController).to(ProductController)
+container.bind<ProductService>(PRODUCT.ProductService).to(ProductService)
+container.bind<ProductRepository>(PRODUCT.ProductRepository).to(ProductRepository)
+container.bind<CategoryRespository>(CATEGORY.categoryRepository).to(CategoryRespository)
+container.bind<CategoryService>(CATEGORY.categoryServices).to(CategoryService)
+container.bind<CategoryController>(CATEGORY.categoryController).to(CategoryController)
 
-export default container
+export default container   
