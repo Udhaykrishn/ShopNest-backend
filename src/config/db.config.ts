435b23748566aeab349915c84990418421722d71
Udhaykrishn <krishnauday320@gmail.com>
2025-02-10 10:24:25 +0530

    fix(db): change the database connection url

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
-import { env } from './env.config';
+import { config} from './env.config';
 
 export class DatabaseService {
     static async connection(): Promise<void> {
         try {
-            await mongoose.connect(env.DATABASE_URL);
+            await mongoose.connect(config.DATABASE_URL);
             console.log('Connected to MongoDB');
         } catch (error) {
             console.error('Database connection error:', error);
