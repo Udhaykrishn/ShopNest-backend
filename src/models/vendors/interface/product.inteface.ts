95e49a2f045a7a75a029e9b4804b57773b728a33
Udhaykrishn <krishnauday320@gmail.com>
2025-02-10 10:33:22 +0530

    fix: update some area of code with type safe

--- src/models/vendors/interface/product.inteface.ts
+++ src/models/vendors/interface/product.inteface.ts
@@ -1,11 +1,17 @@
 import { Document, Types } from "mongoose";
+import { IVariant } from "./varients.interface";
 
 export interface IProduct extends Document {
-    _id: string;
+    _id: Types.ObjectId;
     name: string;
     brand: string;
-    images: [Types.ObjectId];
     description: string;
+    images: string[];
     vendorId: Types.ObjectId;
-    variants: Types.ObjectId[]
+    categoryId: string;
+    variants: IVariant[];
+    price: number;
+    isBlocked: boolean;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
