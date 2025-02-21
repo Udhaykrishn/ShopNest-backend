2690159fa772c2a545f7640fbadbf676f1939c3f
Udhaykrishn <krishnauday320@gmail.com>
2025-02-21 19:25:56 +0530

    feat(interface): update the user interface for make sure the good typo

--- src/models/users/interface/user.interface.ts
+++ src/models/users/interface/user.interface.ts
@@ -1,20 +1,24 @@
-import { Types, Document } from "mongoose"
+import { Document } from "mongoose";
 
 export interface IUser extends Document {
-    email: string;
-    password: string;
-    username: string;
-    avatar?: string;
-    isBlocked: boolean;
-    orders: Types.ObjectId[];
-    wishlist: Types.ObjectId[];
-    payments: Types.ObjectId[];
-    addresses: Types.ObjectId[];
-    wallet: Types.ObjectId;
-    cart: Types.ObjectId;
-    createdAt: Date;
-    updatedAt: Date;
-    role:"user",
-    otp?:string;
-    otpExpires?:Date;
+  _id: string;
+  email: string;
+  googleId: string;
+  password: string;
+  username: string;
+  phone: string;
+  avatar?: string;
+  isBlocked: boolean;
+  orders: string[];
+  wishlist: string[];
+  isVerified: boolean;
+  payments: string[];
+  addresses: string[];
+  wallet: string;
+  cart: string;
+  createdAt: Date;
+  updatedAt: Date;
+  role: "user";
+  otp?: string;
+  otpExpires?: Date;
 }
