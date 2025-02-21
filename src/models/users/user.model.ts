a1dd0f96bd2b40f8f03bdd2c71929138b666e325
Udhaykrishn <krishnauday320@gmail.com>
2025-02-21 19:26:33 +0530

    feat: added user model

--- src/models/users/user.model.ts
+++ src/models/users/user.model.ts
@@ -1,81 +1,74 @@
 import mongoose, { Schema } from "mongoose";
 import { IUser } from "./interface";
+import { passwordToBeHash } from "@/utils";
 
-const UserSchema = new Schema<IUser>({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trime: true,
-        lowercase: true,
+const UserSchema = new Schema<IUser>(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        googleId: {
+            type: String,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 8,
+        },
+        phone: {
+            type: String,
+            trim: true,
+        },
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        isVerified: {
+            type: Boolean
+        },
+        avatar: {
+            type: String,
+            default: null,
+        },
+        isBlocked: {
+            type: Boolean,
+            default: false,
+        },
+        orders: [{ type: String, ref: "Order" }],
+        wishlist: [{ type: String, ref: "Wishlist" }],
+        payments: [{ type: String, ref: "Payment" }],
+        addresses: [{ type: String, ref: "Address" }],
+        wallet: { type: String, ref: "Wallet" },
+        cart: { type: String, ref: "Cart" },
+        role: {
+            type: String,
+            default: "user",
+        },
+        otp: { type: String, required: false },
+        otpExpires: { type: Date, required: false, expires: 300 },
     },
-    password: {
-        type: String,
-        required: true,
-        minlength: 8,
-    },
-    username: {
-        type: String,
-        required: true,
-        trime: true
-    },
-    avatar: {
-        type: String,
-        default: null,
-    },
-    isBlocked: {
-        type: Boolean,
-        default: false,
-    },
-    orders: [{
-        type: Schema.Types.ObjectId,
-        ref: "Order"
-    }],
-    wishlist: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Wishlist'
-    }],
-    payments: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Payment'
-    }],
-    addresses: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Address'
-    }],
-    wallet: {
-        type: Schema.Types.ObjectId,
-        ref: 'Wallet'
-    },
-    cart: {
-        type: Schema.Types.ObjectId,
-        ref: 'Cart'
-    },
-    role: {
-        type: String,
-        default: "user",
-    },
-    otp: { type: String, required: false },
-    otpExpires: { type: Date, required: false, expires: 300 }
-}, { timestamps: true })
+    { timestamps: true }
+);
 
-UserSchema.index({ email: 1 }, { unique: true }),
-    UserSchema.index({ username: 1 })
+UserSchema.index({ username: 1 });
 
 UserSchema.pre("save", async function (next) {
-    const vendor = this as IUser;
-
-    if (!vendor.isModified("password")) return next(); // Only hash if password is modified
+    if (!this.isModified("password")) return next();
 
 
-    this.password = this.password // replace with password hashing method
+    this.password = await passwordToBeHash(this.password);
+
     try {
-
         next();
     } catch (error: any) {
         return next(error);
     }
 });
 
-const User = mongoose.model<IUser>("Users", UserSchema)
-export default User
+const User = mongoose.model<IUser>("Users", UserSchema);
+export default User;
