fddadaadcadf61a3183953c0fecf8bdb7404de04
Udhaykrishn <krishnauday320@gmail.com>
2025-02-21 19:24:17 +0530

    feat: update the admins interface

--- src/models/admins/interface/admin.interface.ts
+++ src/models/admins/interface/admin.interface.ts
@@ -4,8 +4,6 @@ export interface IAdmin extends Document {
     _id: string;
     email: string;
     password: string;
-    categorys: Types.ObjectId[]
-    coupons: Types.ObjectId[]
     createdAt:Date;
     updatedAt:Date,
     role:"admin"
