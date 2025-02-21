d0ae32ff544541ef7e9678af88d2244798959763
Udhaykrishn <krishnauday320@gmail.com>
2025-02-21 19:19:16 +0530

    feat: add the controller modules

--- src/config/inversify.config.ts
+++ src/config/inversify.config.ts
@@ -1,28 +1,18 @@
+import { AdminModule, AuthModule, CategoryModule, OtpModule, ProductModule, ProductsModule, UserModule, VendorModule } from "@/modules";
+import { CategoriesModule } from "@/modules/categories.module";
 import { Container } from "inversify";
-import { VENDOR, USER, CATEGORY } from "@/types"
-import { UserController } from "@/controllers/users/user.controller";
-import { VendorController } from "@/controllers/vendor/vendor.controller";
-import { VendorRepository } from "@/repository/vendor/vendor.repository";
-import { VendorService } from "@/services/vendor/vendor.service";
-import { PRODUCT } from "@/types/product.types";
-import { ProductController } from "@/controllers/vendor/products.controller";
-import { ProductService } from "@/services/vendor/product.services";
-import { ProductRepository } from "@/repository/vendor/product.repository";
-import { CategoryRespository } from "@/repository/admin/category.repository";
-import { CategoryService } from "@/services/admin/category.service";
-import { CategoryController } from "@/controllers/admin/category.controller";
+import { buildProviderModule } from "inversify-binding-decorators";
 
-const container = new Container();
 
-container.bind<UserController>(USER.UserController).to(UserController)
-container.bind<VendorController>(VENDOR.VendorController).to(VendorController)
-container.bind<VendorRepository>(VENDOR.VendorRepository).to(VendorRepository)
-container.bind<VendorService>(VENDOR.VendorService).to(VendorService)
-container.bind<ProductController>(PRODUCT.ProductController).to(ProductController)
-container.bind<ProductService>(PRODUCT.ProductService).to(ProductService)
-container.bind<ProductRepository>(PRODUCT.ProductRepository).to(ProductRepository)
-container.bind<CategoryRespository>(CATEGORY.categoryRepository).to(CategoryRespository)
-container.bind<CategoryService>(CATEGORY.categoryServices).to(CategoryService)
-container.bind<CategoryController>(CATEGORY.categoryController).to(CategoryController)
+const container = new Container({
+    defaultScope: "Singleton",
+    autoBindInjectable: true,
+});
 
-export default container   
+
+container.load(AdminModule, CategoriesModule,VendorModule, ProductsModule, ProductModule, CategoryModule, UserModule, OtpModule, AuthModule);
+
+
+container.load(buildProviderModule());
+
+export default container;
