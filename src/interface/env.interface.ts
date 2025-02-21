ea8d5f4d4639571ec911cdbb5760bc951a5165b5
Udhaykrishn <krishnauday320@gmail.com>
2025-02-21 19:21:30 +0530

    feat: added more env keys with interafce

--- src/interface/env.interface.ts
+++ src/interface/env.interface.ts
@@ -1,6 +1,16 @@
 export interface IEnvConfig {
     // API_URL: string;
     DATABASE_URL: string;
-    // JWT_SECRET: string;
+    JWT_SECRET: string;
     PORT: string;
+    ABSTRACT_EMAIL_API_KEY: string;
+    ABSTRACT_PHONE_API_KEY: string;
+    RESEND_API_KEY: string;
+    GMAIL_APP_ADDRESS: string;
+    GMAIL_APP_PASSWORD: string;
+    FRONT_URL: string;
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
+    GOOGLE_CLIENT_ID: string;
 }
