df78765e73d9198e36c66f74c21560e3e4a4691d
Udhaykrishn <krishnauday320@gmail.com>
2025-02-21 20:13:50 +0530

    feat: creted the user types lke role

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -2,4 +2,8 @@ export const USER = {
     UserRepository: Symbol.for("UserRepository"),
     UserService: Symbol.for("UserService"),
     UserController: Symbol.for("UserController"),
+    EmailService: Symbol.for("EmailService"),
+    OTPService: Symbol.for("OtpService"),
+    OTPRepository: Symbol.for("OTPRepository"),
+    PhoneService: Symbol.for("PhoneService")
 }
