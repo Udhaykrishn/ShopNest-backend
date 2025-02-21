b43d557e77a05c9e5bdfa2433974c9888eb1ca83
Udhaykrishn <krishnauday320@gmail.com>
2025-02-21 19:56:35 +0530

    created the user interface for the repository layer

--- src/repository/interface/user.interface.ts
+++
@@ -1,6 +0,0 @@
-import { IUser } from "@/interface/users.interface";
-
-export interface IUserRepository {
-    getUser(): Promise<IUser>
-    updateOTP(email: string, otp: string, otpExpires: Date): Promise<void>
-}
