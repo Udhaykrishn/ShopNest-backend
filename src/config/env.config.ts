cff210b601eb60f02ea4694afc8cbeacc602e0b0
Udhaykrishn <krishnauday320@gmail.com>
2025-02-21 19:18:33 +0530

    feat: add the env config keys

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -13,5 +13,16 @@ const getEnvVariable = (name: string): string => {
 
 export const config: IEnvConfig = {
     DATABASE_URL: getEnvVariable('DATABASE_URL'),
-    PORT: getEnvVariable('PORT')
+    PORT: getEnvVariable('PORT'),
+    ABSTRACT_EMAIL_API_KEY: getEnvVariable('ABSTRACT_EMAIL_API_KEY'),
+    ABSTRACT_PHONE_API_KEY: getEnvVariable('ABSTRACT_PHONE_API_KEY'),
+    RESEND_API_KEY: getEnvVariable('RESEND_API_KEY'),
+    GMAIL_APP_ADDRESS: getEnvVariable("GMAIL_APP_ADDRESS"),
+    GMAIL_APP_PASSWORD: getEnvVariable("GMAIL_APP_PASSWORD"),
+    JWT_SECRET: getEnvVariable("JWT_SECRET"),
+    FRONT_URL: getEnvVariable("FRONT_URL"),
+    CLOUDINARY_CLOUD_NAME: getEnvVariable("CLOUDINARY_CLOUD_NAME"),
+    CLOUDINARY_API_KEY: getEnvVariable("CLOUDINARY_API_KEY"),
+    CLOUDINARY_API_SECRET: getEnvVariable("CLOUDINARY_API_SECRET"),
+    GOOGLE_CLIENT_ID: getEnvVariable("GOOGLE_CLIENT_ID")
 };
