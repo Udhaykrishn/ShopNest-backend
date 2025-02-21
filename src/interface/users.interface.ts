19107522958f6a3d4fd8362ad026d294255d3c32
Udhaykrishn <krishnauday320@gmail.com>
2025-02-21 19:22:23 +0530

    feat(user): created and update the user interface

--- src/interface/users.interface.ts
+++ src/interface/users.interface.ts
@@ -2,15 +2,20 @@ export interface IUser {
     _id: string;
     email: string;
     password: string;
+    googleId: string;
     isBlocked: boolean;
     createdAt?: Date;
     updatedAt?: Date;
+    phone?: string;
     avatar?: string;
     username: string;
-    addressId: string;
-    cartId: string;
-    walletId: string;
-    orderId: string;
-    wishlistId: string;
-    paymentId: string;
+    addressId?: string;
+    cartId?: string;
+    walletId?: string;
+    orderId?: string;
+    wishlistId?: string;
+    paymentId?: string;
+    otp?: string;
+    otpExpiry?: Date;
+    role:"user"
 }
