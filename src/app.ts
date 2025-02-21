68cced2978e3361f178e3e4042d7f8022550e173
Udhaykrishn <krishnauday320@gmail.com>
2025-02-21 19:17:24 +0530

    feat(main): add middleware and cookie parsers

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,21 @@ import "reflect-metadata"
 import { InversifyExpressServer } from "inversify-express-utils"
 import express from "express"
 import container from "@/config/inversify.config"
-import { config } from "dotenv"
-config()
+import { config } from "@/config"
+import { config as DotConfig } from "dotenv"
+DotConfig()
+import cookie from "cookie-parser"
 import cors from "cors"
+import helmet from "helmet"
 
 const server = new InversifyExpressServer(container)
 
 server.setConfig((app) => {
-    app.use(cors());
+    app.use(cors({ credentials: true, origin: config.FRONT_URL }));
+    app.use(helmet())
+    app.use(cookie())
     app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
 })
 
 const app = server.build();
