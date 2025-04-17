10efa9214ebe6371388b5165bb1f657f19e96622
Udhaykrishn <krishnauday320@gmail.com>
2025-04-17 14:22:23 +0530

    feat: added inversify DI container modules

--- src/config/inversify.config.ts
+++ src/config/inversify.config.ts
@@ -1,18 +1,33 @@
-import { AdminModule, AuthModule, CategoryModule, OtpModule, ProductModule, ProductsModule, UserModule, VendorModule } from "@/modules";
-import { CategoriesModule } from "@/modules/categories.module";
+import {
+    AuthModule,
+    CategoriesModule,
+    OtpModule,
+    ProductsModule,
+    UserModule,
+    VendorModule,
+    CartModule,
+    AddressModule,
+    OrderModule,
+    CheckoutModule
+} from "@/modules";
+import { WishlistModule } from "@/modules/wishlist";
 import { Container } from "inversify";
-import { buildProviderModule } from "inversify-binding-decorators";
-
-
-const container = new Container({
+export const container = new Container({
     defaultScope: "Singleton",
     autoBindInjectable: true,
 });
 
 
-container.load(AdminModule, CategoriesModule,VendorModule, ProductsModule, ProductModule, CategoryModule, UserModule, OtpModule, AuthModule);
-
-
-container.load(buildProviderModule());
-
-export default container;
+container.load(
+    CategoriesModule,
+    VendorModule,
+    ProductsModule,
+    UserModule,
+    CartModule,
+    OtpModule,
+    AuthModule,
+    WishlistModule,
+    OrderModule,
+    AddressModule,
+    CheckoutModule
+);
