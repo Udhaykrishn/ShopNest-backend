38914ec5534545214d692a77fb5ff4ef3c72a433
Udhaykrishn <krishnauday320@gmail.com>
2025-04-17 14:20:27 +0530

    feat: config barrel file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
-export * from "./env.config"
-export * from "./inversify.config"
-export {DatabaseService} from "./db.config"
+export { config } from "./env.config"
+export { container } from "./inversify.config"
+export { DatabaseService } from "./db.config"
