7f3795ca59e0ecb08b3cb7a02e4970f97a0d1624
Udhaykrishn <krishnauday320@gmail.com>
2025-04-17 14:27:19 +0530

    feat: constatns barrel file for re-export functions

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,3 @@
-export * from "./type"
+export { RoleType, OtpType } from './otp.constant';
+export { COOKIE_MAX_AGE, COOKIE_NAME } from "./cookie.constant"
+export { Role } from "./auth-role.constant"
