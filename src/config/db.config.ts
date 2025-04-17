b8bbfd6c47f202eee526635730a91f8812649365
Udhaykrishn <krishnauday320@gmail.com>
2025-04-17 14:17:42 +0530

    feat: db configration

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
-import { config} from './env.config';
+import { config } from './env.config';
 
 export class DatabaseService {
     static async connection(): Promise<void> {
         try {
-            await mongoose.connect(config.DATABASE_URL);
+            await mongoose.connect(config.DATABASE_URL, {
+                maxPoolSize: 100,
+                minPoolSize: 10,
+            });
             console.log('Connected to MongoDB');
         } catch (error) {
             console.error('Database connection error:', error);
