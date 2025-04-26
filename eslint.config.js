c9bc80c146aada26863ab621d71f9a3665821ac2
Udhaykrishn <krishnauday320@gmail.com>
2025-04-26 21:09:45 +0530

    chore: eslint for debug and fix erros and warns

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+// eslint.config.cjs
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config({
+    files: ['**/*.ts'],
+    languageOptions: {
+        parser: tseslint.parser,
+        parserOptions: {
+            project: './tsconfig.json',
+            sourceType: 'module',
+        },
+    },
+    plugins: {
+        '@typescript-eslint': tseslint.plugin,
+    },
+    ignores: ['dist/**', 'node_modules/**'], // replaces .eslintignore
+    rules: {
+        '@typescript-eslint/no-unused-vars': ['warn'],
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+    },
+});
