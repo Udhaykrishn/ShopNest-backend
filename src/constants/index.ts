c153c21acf80127f05435ededaa1b9f4cfd82607
Udhaykrishn <krishnauday320@gmail.com>
2025-04-26 21:04:03 +0530

    feat: wallet enum for the types

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export { RoleType, OtpType } from './otp.constant';
 export { COOKIE_MAX_AGE, COOKIE_NAME } from "./cookie.constant"
 export { Role } from "./auth-role.constant"
+export { OrderStatus } from "./order-status.constant"
