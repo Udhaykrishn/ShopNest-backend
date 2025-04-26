4591aaa614d4c68ce429622cd2829f75190c3e1e
Udhaykrishn <krishnauday320@gmail.com>
2025-04-26 21:14:01 +0530

    feat: order status

---
+++ src/constants/order-status.constant.ts
@@ -0,0 +1,9 @@
+export enum OrderStatus {
+    PROCESSING = "processing",
+    SHIPPED = "shipped",
+    DELIVERED = "delivered",
+    CANCELLED = "cancelled",
+    RETURNED = "returned"
+}
+
+export const COMMISION_RATE = 0.10;
