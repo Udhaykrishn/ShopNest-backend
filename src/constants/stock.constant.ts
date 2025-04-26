633e071ec3dbf9ef7bd45b14a56941e3b8f94ef6
Udhaykrishn <krishnauday320@gmail.com>
2025-04-26 21:15:11 +0530

    feat: constant for stock propertys

---
+++ src/constants/stock.constant.ts
@@ -0,0 +1,6 @@
+export const STOCK_MAX_LIMIT_PER_ORDER = 5
+export const STOCK_MIN_LIMIT_PER_ORDER = 0
+export const STOCK_INCREASE_COUNT = 1
+export const STOCK_DECREASE_COUNT = 1
+export const STOCK_INCREASE_ACTION = "increase"
+export const STOCK_DECREASE_ACTION = "decrease"
