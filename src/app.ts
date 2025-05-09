fcb48e48cfef4513fb77c76a14669521d2014546
UdhayKrishn <krishnauday320@gmail.com>
2025-05-10 00:48:00 +0530

    feat: hpp middleaare for http parameter security guard

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,34 @@
-import "reflect-metadata"
-import { InversifyExpressServer } from "inversify-express-utils"
-import express from "express"
-import { config, container } from "@/config"
-import { config as DotConfig } from "dotenv"
-DotConfig()
-import cookie from "cookie-parser"
-import cors from "cors"
-import helmet from "helmet"
-import compression from "compression"
+import "reflect-metadata";
+import { InversifyExpressServer } from "inversify-express-utils";
+import express from "express";
+import { config, container } from "@/config";
+import { config as DotConfig } from "dotenv";
+DotConfig();
+import cookie from "cookie-parser";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import hpp from "hpp";
 
-const server = new InversifyExpressServer(container)
+const server = new InversifyExpressServer(container);
+
 server.setConfig((app) => {
-    app.use(helmet())
-    app.use(cors({ credentials: true, origin: config.FRONT_URL }));
-    app.use(express.json())
-    app.use(cookie()) 
-    app.use(compression())
-})
+    app.use(helmet());
+
+    app.use(compression());
+
+    app.use(cors({
+        credentials: true,
+        origin: config.FRONT_URL,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    }));
+
+    app.use(hpp());
+
+    app.use(express.json());
+    app.use(cookie());
+});
 
 const app = server.build();
-
 export default app;
