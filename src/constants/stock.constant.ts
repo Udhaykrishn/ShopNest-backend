eaf63d47dee9821f9aac6c44a05b77d98256c41b
UdhayKrishn <krishnauday320@gmail.com>
2025-05-28 13:07:27 +0530

    feat: stock constant

--- src/constants/stock.constant.ts
+++ src/constants/stock.constant.ts
@@ -3,4 +3,4 @@ export const STOCK_MIN_LIMIT_PER_ORDER = 0
 export const STOCK_INCREASE_COUNT = 1
 export const STOCK_DECREASE_COUNT = 1
 export const STOCK_INCREASE_ACTION = "increase"
-export const STOCK_DECREASE_ACTION = "decrease"
+export const STOCK_DECREASE_ACTION = "deincrease"
