387110b8d188170978c467db11906451fe6580e3
UdhayKrishn <krishnauday320@gmail.com>
2025-05-28 15:16:15 +0530

    feat: re-export all interface modules in index

---
+++ src/interface/index.ts
@@ -0,0 +1,17 @@
+export { IVendors } from "./vendor.interface"
+export { IUser } from "./users.interface"
+export { IProduct } from "./product.interface"
+export { IEnvConfig } from "./env.interface"
+export { IEmailService } from "./email.interface"
+export { ICategory } from "./category.interface"
+export { Cart as ICart } from "./cart/cart.interface"
+export { IWishlist } from "./wishlist"
+export { AddressItem, IAddress } from "./address/address.interface"
+export { ICoupon, ICouponUsage } from "./coupon"
+export { IOffers } from "./offers"
+export {
+    IBaseWallet,
+    IUserWallet,
+    IVendorWallet,
+    paymentType
+} from "./wallet/vendor-wallet.interface"
