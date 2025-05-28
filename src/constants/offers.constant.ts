c4618695a83dc8e96e4b9d77ceb904705f2c2799
UdhayKrishn <krishnauday320@gmail.com>
2025-05-28 12:55:43 +0530

    feat: offers enums type status and ranges of offers

---
+++ src/constants/offers.constant.ts
@@ -0,0 +1,16 @@
+export enum OffersType {
+    CATEGORY = "category",
+    REFERAL = "referal"
+}
+
+export enum OfferStatus {
+    ACTIVE = "active",
+    PENDING = "pending",
+    EXPIRED = "expired",
+    BLOCKED = "blocked"
+}
+
+export enum OfferDiscountRange{
+    MIN_RANGE = 100,
+    MAX_RANGE = 1000
+}
