d8b56fb2456cb75fbed80ede43dcde8a725138bd
UdhayKrishn <krishnauday320@gmail.com>
2025-05-28 12:48:59 +0530

    feat: change daatabase connection method to connect method

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { DatabaseService ,config} from "@/config"
 const PORT = config.PORT;
 
 const bootstrap = async () => {
-  await DatabaseService.connection();
+  await DatabaseService.connect();
   app.listen(PORT, () => {
     console.log(`🚀 Server started on http://localhost:${PORT}, PID: ${process.pid}`);
   });
