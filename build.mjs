4f5a6751314218e872f3c766a38fddc94c9d5498
UdhayKrishn <krishnauday320@gmail.com>
2025-06-09 13:50:15 +0530

    chore: development config

--- build.mjs
+++ build.mjs
@@ -2,27 +2,27 @@ import { build } from 'esbuild';
 import aliasPath from 'esbuild-plugin-alias-path';
 
 build({
-    entryPoints: ['src/index.ts'],
-    bundle: true,
-    platform: 'node',
-    format: 'cjs',
-    sourcemap: false,
-    minify: true,
-    target: ["node18"],
-    outdir: 'dist/index.cjs',
-    plugins: [
-        aliasPath({
-            alias: {
-                '@': './src',
-            },
-        }),
-    ],
-    external: [
-        'express',
-        'body-parser',
-        'depd',
-        'inversify-express-utils',
-        "argon2",
-        "mongoose"
-    ],
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  platform: 'node',
+  format: 'cjs',
+  sourcemap: true,           
+  minify: false,               
+  target: ['node20'],
+  outdir: 'dist',
+  plugins: [
+    aliasPath({
+      alias: {
+        '@': './src',
+      },
+    }),
+  ],
+  external: [
+    'express',
+    'body-parser',
+    'depd',
+    'inversify-express-utils',
+    'argon2',
+    'mongoose',
+  ],
 }).catch(() => process.exit(1));
