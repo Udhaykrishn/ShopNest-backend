c48855ca9180e28856fd07156b2a0b6e80efd10c
UdhayKrishn <krishnauday320@gmail.com>
2025-06-09 13:54:32 +0530

    feat: config all project setup

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,14 +1,17 @@
 import mongoose from 'mongoose';
 import { config } from './env.config';
+import { POOLSIZE } from "../constants/pool-size.constant"
+import { Product } from '@/models/vendors/implements';
 
 export class DatabaseService {
     static async connect(): Promise<void> {
         try {
-            await mongoose.connect(config.DATABASE_URL, {
-                maxPoolSize: 100,
-                minPoolSize: 10,
+            const { connection: { host,name } } = await mongoose.connect(config.DATABASE_URL, {
+                dbName:"ecommerce",
+                maxPoolSize: POOLSIZE.MAX,
+                minPoolSize: POOLSIZE.MIN,
             });
-            console.log('Connected to MongoDB');
+            console.log('Connected to MongoDB',host,name);
         } catch (error) {
             console.error('Database connection error:', error);
             process.exit(1);
